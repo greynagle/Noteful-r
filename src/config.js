c72c7bf7034167758cae9ef5504135707b16aedc
Grey Nagle <greynagle@gmail.com>
2020-06-14 23:26:52 -0400

    updates

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://shrouded-woodland-00278.herokuapp.com/',
+  API_ENDPOINT: 'https://shrouded-woodland-00278.herokuapp.com',
   API_TOKEN: process.env.API_TOKEN || '123',
 }
